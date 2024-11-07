0f768d61dfa6043caa18a8f7a74177446df1f73b
WAHU0508 <sharongikenye@gmail.com>
2024-11-07 11:26:05 +0300

    Add a files section

--- src/components/FileRows.js
+++ src/components/FileRows.js
@@ -1,12 +1,13 @@
 import React from 'react'
-
+import file_icon from '../assets/file_icon.png'
+import more_vert from '../assets/more_vert.png'
 function FileRows() {
   return (
     <tr>
-        <td>Image</td>
+        <td><img src={file_icon} alt="file_icon" id="file_icon"/></td>
         <td>Doc1</td>
         <td>Google Docs</td>
-        <td>Options</td>
+        <td><img src={more_vert} alt="more_vert" id="more_vert"/></td>
     </tr>
   )
 }
